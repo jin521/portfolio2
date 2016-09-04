a0277d9666940e59cb23795d762ebee13be7aec5
jin521 <jinlc220@126.com>
2016-09-05 00:48:34 +1000

    edit button

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@
 
 $(function(){
         $(".screen-typed").typed({
-            strings: ["> Hello! ^1000 \n> I am Jin ^600 \n> I am a full stack web developper based in Sydney \n> // say something cleaver here...^1500 \n> I love to make /create things,\n> Web apps, paintings, photoes and ^1000cocktails... \n> Start scrolling to learn more about me. "],
+            strings: ["> Hello! ^1000 \n> I am Jin ^600 \n> I am a full stack web developper based in Sydney \n> // say something cleaver here...^1500 \n> I love to make /create things,\n> Web apps, paintings, photoes and ^1000cocktails... \n> Start scrolling to learn more about me "],
             typeSpeed: 20
         })
     });
