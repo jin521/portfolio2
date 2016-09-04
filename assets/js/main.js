a3fdc572136aad1faa670df97cb98f659c6fe7be
jin521 <jinlc220@126.com>
2016-09-04 23:09:03 +1000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+
+
+
+$(function(){
+        $(".screen-typed").typed({
+            strings: ["> Hello! ^1000 \n> I am Jin ^600 \n> I am a full stack web developper based in Sydney \n> // say something cleaver here...^1500 \n> I love to make /create things,\n> Web apps, paintings, photoes and ^1000cocktails... \n> Start scrolling to learn more about me. "],
+            typeSpeed: 20
+        })
+    });
+
+
+
+		$(document).ready(function() {
+
+		$(".profilepic").mouseover(function(){
+
+		      $(this).css({
+		        "opacity": "0.5",
+		        "width": "150px",
+		        "height": "150px",
+		      });
+		});
+
+		$(".profilepic").mouseout(function(){
+
+		      $(this).css({
+		        "opacity": "1",
+		        "width": "120px",
+		        "height": "120px",
+		      });
+		});
+
+
+		$(".grid").mouseover(function(){
+
+		      $(this).css( "opacity", "1");
+		});
+
+		$(".grid").mouseout(function(){
+
+	      $(this).css( "opacity", "0.5");
+
+		});
+
+		});
